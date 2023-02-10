619208e4a6138e06b8cd937fa4bf1d4d7477d91d
siva-kumar-22 <111413890+siva-kumar-22@users.noreply.github.com>
2023-02-10 07:57:57 +0530

    Update hello_world.java

--- hello_world.java
+++ hello_world.java
@@ -2,5 +2,6 @@ class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.println("this is my new java program");
+        //http://54.162.165.25:8080/
     }
 }
